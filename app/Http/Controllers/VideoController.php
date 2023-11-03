c5141d401128ab03533729570742921f506d3dd5
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-03 20:10:47 +0200

    Model, Controller and Migration for Video added

---
+++ app/Http/Controllers/VideoController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+
+class VideoController extends Controller
+{
+    function home() {
+        $data = Video::all();
+        return view("home");
+    }
+}
