6389763d091cc2ffc2fc93af11409a525b604ba8
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-04 09:06:12 +0200

    Minor changes made

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,10 @@
             <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" style="color: #0606A7; width: 100%; background-color: #D6DCD7; border-radius: 32px;">
                 @foreach ($videos as $rw)
                     <div class="card" style="width: 18rem; padding: 10px; margin: 10px;">
-                        <img src="/{{ $rw->video_image }}.jpg" class="card-img-top" alt="...">
+                        <img  src="{{ asset('/storage/video_images/' . $rw->video_image) }} "
+                          class="card-img-top"
+                          style="width: 260px; height: 200px;"
+                          alt="...">
                         <div class="card-body">
                             <h5 class="card-title">{{ $rw->title_of_video }}</h5>
                             <p class="card-text">{{ $rw->user }}</p>
