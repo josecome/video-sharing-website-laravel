dadfebd1484d20b1ea0725db8d373a315b45e8d8
Jose Jaime Come <josejaimecome@gmail.com>
2023-11-13 20:47:53 +0200

    ApiController, Cors Middleware, Comment model and Resources added

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Video;
+use Illuminate\Http\Request;
+use App\Http\Resources\VideoResource;
+
+class ApiController extends Controller
+{
+    function home() {
+        $data = Video::limit(8)->get();
+        return json_decode($data);
+    }
+    function video(int $id) {
+        return new VideoResource(Video::findOrFail($id));
+    }
+}
---
+++ app/Http/Resources/VideoResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\CommentResource;
+
+class VideoResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'title_of_video' => $this->title_of_video,
+            'description' => $this->description,
+            'video_image' => $this->description,
+            'views' => $this->views,
+            'posts' => CommentResource::collection($this->comments),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
